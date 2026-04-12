de6cc61ef2354b2a241f39c61f84a712cc64d38b
PeterC <petercalifano.gs@gmail.com>
2026-04-12 17:53:58 +0200

    Update path

--- display_dual_gp12_target_rail.sh
+++ display_dual_gp12_target_rail.sh
@@ -1,3 +1,3 @@
 source /opt/ros/jazzy/setup.bash
 source ./install/setup.bash
-ros2 launch ./src/cosmica_resources/launch/view_gp12_dual_rail_target.launch.xml
+ros2 launch ./src/motoman_gp12_dual_cell_support/launch/view_gp12_dual_rail_target.launch.xml
